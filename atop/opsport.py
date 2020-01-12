69b33f0a7217046e35946d18b2fe97220d0a7240
M Nef <howtoeatpineapples@gmail.com>
2020-01-12 09:37:02 +0800

    added better structure to OptionPortfolio collection

--- atop/opsport.py
+++ atop/opsport.py
@@ -1,14 +1,23 @@
-class OptionPortfolio:
-    def __init__(self, assetlist):
-        self.assetlist = assetlist
+class OptionPortfolio(collections.abc.Collection):
+    def __init__(self):
+        self.assetlist = []
 
-    def add_asset(self):
-        pass
+    def insert(self, value):
+        assert isinstance(value, asset)
+        self.assetlist.append(value)
 
-    def remove_asset(self):
+    def remove(self, _asset):
         pass
 
     def calc_graph(self):
         pass
 
+    def __contains__(self, value):
+        return value in self.assetlist
+
+    def __iter__(self):
+        return iter(self.assetlist)
+
+    def __len__(self):
+        return len(self.assetlist)
     
