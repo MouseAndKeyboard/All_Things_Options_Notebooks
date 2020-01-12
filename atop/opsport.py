ffc1b893c55e6e774c97025c2673dccdeebadc0d
M Nef <howtoeatpineapples@gmail.com>
2020-01-12 10:28:19 +0800

    added dummy code to explain what could be done with the options portfolio

--- atop/opsport.py
+++ atop/opsport.py
@@ -42,4 +42,39 @@ class OptionPortfolio(Collection):
 
     def __len__(self):
         return len(self.assetlist)
+
+
+
+
+if __name__ == "__main__":
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from atop.options.calloption import CallOption, CallOptionLong
+    from atop.options.calloption import PutOptionShort, PutOptionLong
+
+    # Create some options
+    Call_1 = CallOption(23.00, 24.00, None, None, None) # I don't know what all the parameters are
+    Call_2 = CallOption(23.00, 26.00, None, None, None) # I don't know what all the parameters are
+    Call_3 = CallOption(23.00, 28.00, None, None, None) # I don't know what all the parameters are
+
+    # Create your portfolio
+    my_portfolio = OptionPortfolio()
     
+    # Add options to the portfolio
+    my_portfolio.insert(Call_1)
+    my_portfolio.insert(Call_2)
+    my_portfolio.insert(Call_3)
+
+    # Create an array of 100 points between 0 to 120
+    x = np.linspace(0, 120, num=100)
+    # Apply the profit function at each underlying value
+    y = my_portfolio.total_payoff(x)
+
+    # Plot the profit function for each underlying value
+    plt.plot(x, y)
+
+    plt.xlabel('Underlying Value')
+    plt.ylabel('Profit')
+    plt.legend()
+    plt.grid(True)
+    plt.show(block = True)
